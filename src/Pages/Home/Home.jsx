c2bc7d5ecaef5934241531ac22af5917955f8eda
DevTabibur <tobiburrohman2@gmail.com>
2022-05-01 23:01:14 +0600

    show product home done

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,16 +1,43 @@
-import React, { useContext } from 'react';
-import './Home.css';
-import HeroBanner from '../Banner/HeroBanner';
-import { ProductContext } from '../../App';
+import React, { useContext } from "react";
+import "./Home.css";
+import HeroBanner from "../Banner/HeroBanner";
+import { ProductContext } from "../../App";
+import { Button, Card, Col, Container, Row } from "react-bootstrap";
 
 const Home = () => {
-    const [products, setProducts] = useContext(ProductContext);
-    console.log(products);
+  const [products, setProducts] = useContext(ProductContext);
+  // const sliceProducts = products.slice(0, 6);
+  // console.log(sliceProducts);
   return (
     <>
-        {/* <HeroBanner/> */}
+      {/* <HeroBanner/> */}
+      <Container>
+        <Row>
+          {products.map((product) => (
+            <ShowProductHome key={product.id} product={product} />
+          ))}
+        </Row>
+      </Container>
     </>
-  )
-}
+  );
+};
 
-export default Home
+const ShowProductHome = ({ product }) => {
+  return (
+    <Col className="d-flex justify-content-center align-items-center">
+      <Card style={{ width: "18rem" }}>
+        <Card.Img variant="top" src={product.image} />
+        <Card.Body>
+          <Card.Title>{product.name}</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
+    </Col>
+  );
+};
+
+export default Home;
